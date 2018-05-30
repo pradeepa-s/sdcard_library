07ac0a03e373f0342251ab26316d063711a314aa
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2018-05-30 13:19:17 +1000

    dir_register function changed according to MISRA R warnings: 1020

--- example/tms570ls11x/CCS/sdcard_test/fatfs/src/ff.c
+++ example/tms570ls11x/CCS/sdcard_test/fatfs/src/ff.c
@@ -584,6 +584,7 @@ static DWORD create_chain (FFOBJID* obj, DWORD clst);
 static FRESULT dir_clear (FATFS *fs, DWORD clst);
 static FRESULT dir_sdi (DIR* dp, DWORD ofs);
 static FRESULT dir_next (DIR* dp, int stretch);
+static FRESULT dir_alloc (DIR* dp, UINT nent);
 static FRESULT dir_read (DIR* dp, int vol);
 static FRESULT dir_find (DIR* dp);
 static FRESULT dir_register (DIR* dp);
@@ -2983,90 +2984,127 @@ static FRESULT dir_register (	/* FR_OK:succeeded, FR_DENIED:no free entry or too
 	FATFS *fs = dp->obj.fs;
 #if FF_USE_LFN		/* LFN configuration */
 	UINT n, nlen, nent;
-	BYTE sn[12], sum;
+	BYTE sn[12] = {0}, sum;
 
 
-	if (dp->fn[NSFLAG] & (NS_DOT | NS_NONAME)) return FR_INVALID_NAME;	/* Check name validity */
-	for (nlen = 0; fs->lfnbuf[nlen]; nlen++) ;	/* Get lfn length */
+	if ((dp->fn[NSFLAG]) & (NS_DOT | NS_NONAME)) {
+	    res = FR_INVALID_NAME;	/* Check name validity */
+	}
+	else{
+	    for (nlen = 0U; fs->lfnbuf[nlen]; nlen++){ /* Get lfn length */
+        }
 
 #if FF_FS_EXFAT
-	if (fs->fs_type == FS_EXFAT) {	/* On the exFAT volume */
-		nent = (nlen + 14) / 15 + 2;	/* Number of entries to allocate (85+C0+C1s) */
-		res = dir_alloc(dp, nent);		/* Allocate entries */
-		if (res != FR_OK) return res;
-		dp->blk_ofs = dp->dptr - SZDIRE * (nent - 1);	/* Set the allocated entry block offset */
+        if (fs->fs_type == FS_EXFAT) {  /* On the exFAT volume */
+            nent = (nlen + 14) / 15 + 2;    /* Number of entries to allocate (85+C0+C1s) */
+            res = dir_alloc(dp, nent);      /* Allocate entries */
+            if (res != FR_OK){
+                return res;
+            }
+            dp->blk_ofs = dp->dptr - SZDIRE * (nent - 1);   /* Set the allocated entry block offset */
 
-		if (dp->obj.stat & 4) {			/* Has the directory been stretched? */
-			dp->obj.stat &= ~4;
-			res = fill_first_frag(&dp->obj);	/* Fill the first fragment on the FAT if needed */
-			if (res != FR_OK) return res;
-			res = fill_last_frag(&dp->obj, dp->clust, 0xFFFFFFFF);	/* Fill the last fragment on the FAT if needed */
-			if (res != FR_OK) return res;
-			if (dp->obj.sclust != 0) {		/* Is it a sub directory? */
-				DIR dj;
+            if (dp->obj.stat & 4) {         /* Has the directory been stretched? */
+                dp->obj.stat &= ~4;
+                res = fill_first_frag(&dp->obj);    /* Fill the first fragment on the FAT if needed */
+                if (res != FR_OK){
+                    return res;
+                }
+                res = fill_last_frag(&dp->obj, dp->clust, 0xFFFFFFFF);  /* Fill the last fragment on the FAT if needed */
+                if (res != FR_OK){
+                    return res;
+                }
+                if (dp->obj.sclust != 0) {      /* Is it a sub directory? */
+                    DIR dj;
 
-				res = load_obj_xdir(&dj, &dp->obj);	/* Load the object status */
-				if (res != FR_OK) return res;
-				dp->obj.objsize += (DWORD)fs->csize * SS(fs);			/* Increase the directory size by cluster size */
-				st_qword(fs->dirbuf + XDIR_FileSize, dp->obj.objsize);	/* Update the allocation status */
-				st_qword(fs->dirbuf + XDIR_ValidFileSize, dp->obj.objsize);
-				fs->dirbuf[XDIR_GenFlags] = dp->obj.stat | 1;
-				res = store_xdir(&dj);				/* Store the object status */
-				if (res != FR_OK) return res;
-			}
-		}
+                    res = load_obj_xdir(&dj, &dp->obj); /* Load the object status */
+                    if (res != FR_OK){
+                        return res;
+                    }
+                    dp->obj.objsize += (DWORD)fs->csize * SS(fs);           /* Increase the directory size by cluster size */
+                    st_qword(fs->dirbuf + XDIR_FileSize, dp->obj.objsize);  /* Update the allocation status */
+                    st_qword(fs->dirbuf + XDIR_ValidFileSize, dp->obj.objsize);
+                    fs->dirbuf[XDIR_GenFlags] = dp->obj.stat | 1;
+                    res = store_xdir(&dj);              /* Store the object status */
+                    if (res != FR_OK){
+                        return res;
+                    }
+                }
+            }
 
-		create_xdir(fs->dirbuf, fs->lfnbuf);	/* Create on-memory directory block to be written later */
-		return FR_OK;
-	}
+            create_xdir(fs->dirbuf, fs->lfnbuf);    /* Create on-memory directory block to be written later */
+            return FR_OK;
+        }
 #endif
-	/* On the FAT/FAT32 volume */
-	mem_cpy(sn, dp->fn, 12);
-	if (sn[NSFLAG] & NS_LOSS) {			/* When LFN is out of 8.3 format, generate a numbered name */
-		dp->fn[NSFLAG] = NS_NOLFN;		/* Find only SFN */
-		for (n = 1; n < 100; n++) {
-			gen_numname(dp->fn, sn, fs->lfnbuf, n);	/* Generate a numbered name */
-			res = dir_find(dp);				/* Check if the name collides with existing SFN */
-			if (res != FR_OK) break;
-		}
-		if (n == 100) return FR_DENIED;		/* Abort if too many collisions */
-		if (res != FR_NO_FILE) return res;	/* Abort if the result is other than 'not collided' */
-		dp->fn[NSFLAG] = sn[NSFLAG];
-	}
+        /* On the FAT/FAT32 volume */
+        mem_cpy(sn, dp->fn, 12U);
 
-	/* Create an SFN with/without LFNs. */
-	nent = (sn[NSFLAG] & NS_LFN) ? (nlen + 12) / 13 + 1 : 1;	/* Number of entries to allocate */
-	res = dir_alloc(dp, nent);		/* Allocate entries */
-	if (res == FR_OK && --nent) {	/* Set LFN entry if needed */
-		res = dir_sdi(dp, dp->dptr - nent * SZDIRE);
-		if (res == FR_OK) {
-			sum = sum_sfn(dp->fn);	/* Checksum value of the SFN tied to the LFN */
-			do {					/* Store LFN entries in bottom first */
-				res = move_window(fs, dp->sect);
-				if (res != FR_OK) break;
-				put_lfn(fs->lfnbuf, dp->dir, (BYTE)nent, sum);
-				fs->wflag = 1;
-				res = dir_next(dp, 0);	/* Next entry */
-			} while (res == FR_OK && --nent);
-		}
-	}
+        if (sn[NSFLAG] & NS_LOSS) {         /* When LFN is out of 8.3 format, generate a numbered name */
+            dp->fn[NSFLAG] = NS_NOLFN;      /* Find only SFN */
+            for (n = 1U; n < 100U; n++) {
+                gen_numname(dp->fn, sn, fs->lfnbuf, n); /* Generate a numbered name */
+                res = dir_find(dp);             /* Check if the name collides with existing SFN */
+                if (res != FR_OK){
+                    break;
+                }
+            }
 
-#else	/* Non LFN configuration */
-	res = dir_alloc(dp, 1);		/* Allocate an entry for SFN */
+            if (n == 100U){
+                res = FR_DENIED;       /* Abort if too many collisions */
+            }
+            else if(res == FR_NO_FILE){
+                dp->fn[NSFLAG] = sn[NSFLAG];
+            }
+            else{
+                /* Abort if the result is other than 'not collided' */
+            }
+        }
 
-#endif
+        if(res == FR_NO_FILE){
 
-	/* Set SFN entry */
-	if (res == FR_OK) {
-		res = move_window(fs, dp->sect);
-		if (res == FR_OK) {
-			mem_set(dp->dir, 0, SZDIRE);	/* Clean the entry */
-			mem_cpy(dp->dir + DIR_Name, dp->fn, 11);	/* Put SFN */
-#if FF_USE_LFN
-			dp->dir[DIR_NTres] = dp->fn[NSFLAG] & (NS_BODY | NS_EXT);	/* Put NT flag */
-#endif
-			fs->wflag = 1;
-		}
+            /* Create an SFN with/without LFNs. */
+            nent = (sn[NSFLAG] & NS_LFN) ? (nlen + 12U) / 13U + 1U : 1U;    /* Number of entries to allocate */
+            res = dir_alloc(dp, nent);      /* Allocate entries */
+            if(res == FR_OK){
+                if (--nent) {   /* Set LFN entry if needed */
+                    res = dir_sdi(dp, dp->dptr - nent * SZDIRE);
+                    if (res == FR_OK) {
+                        sum = sum_sfn(dp->fn);  /* Checksum value of the SFN tied to the LFN */
+                        do {                    /* Store LFN entries in bottom first */
+                            res = move_window(fs, dp->sect);
+                            if (res != FR_OK){
+                                break;
+                            }
+                            put_lfn(fs->lfnbuf, dp->dir, (BYTE)nent, sum);
+                            fs->wflag = 1U;
+                            res = dir_next(dp, 0);  /* Next entry */
+
+                            if(res == FR_OK){
+                                --nent;
+                            }
+
+                        } while ((res == FR_OK) && (nent));
+                    }
+                }
+            }
+
+        #else   /* Non LFN configuration */
+            res = dir_alloc(dp, 1);     /* Allocate an entry for SFN */
+
+        #endif
+
+            /* Set SFN entry */
+            if (res == FR_OK) {
+                res = move_window(fs, dp->sect);
+                if (res == FR_OK) {
+                    mem_set(dp->dir, 0U, SZDIRE);   /* Clean the entry */
+                    mem_cpy(dp->dir + DIR_Name, dp->fn, 11U);   /* Put SFN */
+        #if FF_USE_LFN
+                    dp->dir[DIR_NTres] = dp->fn[NSFLAG] & (NS_BODY | NS_EXT);   /* Put NT flag */
+        #endif
+                    fs->wflag = 1U;
+                }
+            }
+        }
 	}
 
 	return res;
