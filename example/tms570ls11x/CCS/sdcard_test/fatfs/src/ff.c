7e32d23aa4546a3eebe901cc8040d2e0de173fa3
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2018-06-05 15:47:02 +1000

    f_write modified according to MISRA Warnings R: 251

--- example/tms570ls11x/CCS/sdcard_test/fatfs/src/ff.c
+++ example/tms570ls11x/CCS/sdcard_test/fatfs/src/ff.c
@@ -458,7 +458,7 @@ static const char* const VolumeStr[FF_VOLUMES] = {FF_VOLUME_STRS};	/* Pre-define
 #define LEAVE_MKFS(res)	return res
 
 #else					/* LFN configurations */
-#if FF_MAX_LFN < 12 || FF_MAX_LFN > 255
+#if FF_MAX_LFN < 12U || FF_MAX_LFN > 255U
 #error Wrong setting of FF_MAX_LFN
 #endif
 #if FF_LFN_BUF < FF_SFN_BUF || FF_SFN_BUF < 12
@@ -3356,6 +3356,9 @@ static void get_fileinfo (
                 si = di;
 
                 while (fs->lfnbuf[si] != 0U) {
+
+                    loop_skip = 0U;
+
                     wc = fs->lfnbuf[si++];		/* Get an LFN character (UTF-16) */
 
                     if(hs == 0U){   /* Is it a surrogate? */
@@ -3674,6 +3677,8 @@ static FRESULT create_name (	/* FR_OK: successful, FR_INVALID_NAME: could not cr
             ni = 8u;
 
             for (;;) {
+                loop_skip = 0U;
+
                 wc = lfn[si++];					/* Get an LFN character */
 
                 if (wc == 0U){
@@ -4988,104 +4993,263 @@ FRESULT f_write (
 	DWORD clst, sect;
 	UINT wcnt, cc, csect;
 	const BYTE *wbuff = (const BYTE*)buff;
+	BYTE loop_skip = 0U, loop_exit = 0U, func_exit = 0U;
 
+	FSIZE_t temp_fsizet = 0U;
+    WORD temp_word = 0U;
 
 	*bw = 0U;	/* Clear write byte counter */
 	res = validate(&fp->obj, &fs);			/* Check validity of the file object */
-	if ((res != FR_OK) || ((res = (FRESULT)fp->err) != FR_OK)) LEAVE_FF(fs, res);	/* Check validity */
-	if (!(fp->flag & FA_WRITE)) LEAVE_FF(fs, FR_DENIED);	/* Check access mode */
 
-	/* Check fptr wrap-around (file size cannot reach 4 GiB at FAT volume) */
-	if (((!FF_FS_EXFAT) || (fs->fs_type != FS_EXFAT)) && ((DWORD)(fp->fptr + btw) < (DWORD)fp->fptr)) {
-		btw = (UINT)(0xFFFFFFFF - (DWORD)fp->fptr);
+
+	if (res != FR_OK){  /* Check validity */
+	    func_exit = 1U;
+	    /* Exit */
 	}
+	else if(((FRESULT)fp->err) != FR_OK){   /* Check validity */
+	    res = (FRESULT)fp->err;
+	    func_exit = 1U;
+	    /* Exit */
+	}
+	else if(!(fp->flag & FA_WRITE)){    /* Check access mode */
+	    res = FR_DENIED;
+	    func_exit = 1U;
+	    /* Exit */
+	}
+	else{
+        /* Check fptr wrap-around (file size cannot reach 4 GiB at FAT volume) */
+        if (((!FF_FS_EXFAT) || (fs->fs_type != FS_EXFAT)) && ((DWORD)(fp->fptr + btw) < (DWORD)fp->fptr)) {
+            btw = (UINT)(0xFFFFFFFF - (DWORD)fp->fptr);
+        }
 
-	for ( ;  btw;							/* Repeat until all data written */
-		btw -= wcnt, *bw += wcnt, wbuff += wcnt, fp->fptr += wcnt, fp->obj.objsize = (fp->fptr > fp->obj.objsize) ? fp->fptr : fp->obj.objsize) {
-		if (fp->fptr % SS(fs) == 0U) {		/* On the sector boundary? */
-			csect = (UINT)(fp->fptr / SS(fs)) & (fs->csize - 1U);	/* Sector offset in the cluster */
-			if (csect == 0U) {				/* On the cluster boundary? */
-				if (fp->fptr == 0U) {		/* On the top of the file? */
-					clst = fp->obj.sclust;	/* Follow from the origin */
-					if (clst == 0U) {		/* If no cluster is allocated, */
-						clst = create_chain(&fp->obj, 0);	/* create a new cluster chain */
-					}
-				} else {					/* On the middle or end of the file */
-#if FF_USE_FASTSEEK
-					if (fp->cltbl) {
-						clst = clmt_clust(fp, fp->fptr);	/* Get cluster# from the CLMT */
-					} else
-#endif
-					{
-						clst = create_chain(&fp->obj, fp->clust);	/* Follow or stretch cluster chain on the FAT */
-					}
-				}
-				if (clst == 0U) break;		/* Could not allocate a new cluster (disk full) */
-				if (clst == 1U) ABORT(fs, FR_INT_ERR);
-				if (clst == 0xFFFFFFFFU) ABORT(fs, FR_DISK_ERR);
-				fp->clust = clst;			/* Update current cluster */
-				if (fp->obj.sclust == 0U) fp->obj.sclust = clst;	/* Set start cluster if the first write */
-			}
+        for ( ;  btw; btw -= wcnt) {    /* Repeat until all data written */
+
+            loop_skip = 0U;
+
+            if (fp->fptr % SS(fs) == 0U) {		/* On the sector boundary? */
+
+                temp_fsizet = fp->fptr;
+                temp_fsizet = temp_fsizet / SS(fs);
+
+                temp_word = fs->csize;
+                temp_word -= 1U;
+
+                csect = (UINT)(temp_fsizet & temp_word);	/* Sector offset in the cluster */
+                if (csect == 0U) {				/* On the cluster boundary? */
+                    if (fp->fptr == 0U) {		/* On the top of the file? */
+                        clst = fp->obj.sclust;	/* Follow from the origin */
+                        if (clst == 0U) {		/* If no cluster is allocated, */
+                            clst = create_chain(&fp->obj, 0U);	/* create a new cluster chain */
+                        }
+                    } else {					/* On the middle or end of the file */
+    #if FF_USE_FASTSEEK
+                        if (fp->cltbl) {
+                            clst = clmt_clust(fp, fp->fptr);	/* Get cluster# from the CLMT */
+                        } else
+    #endif
+                        {
+                            clst = create_chain(&fp->obj, fp->clust);	/* Follow or stretch cluster chain on the FAT */
+                        }
+                    }
+
+                    if (clst == 0U){
+                        loop_exit = 1U;
+                        /* Could not allocate a new cluster (disk full) */
+                    }
+                    else if (clst == 1U){
+                        loop_exit = 1U;
+                        func_exit = 1U;
+
+                        res = FR_INT_ERR;
+                        fp->err = (BYTE)(res);
+
+                        /* Exit */
+                    }
+                    else if (clst == 0xFFFFFFFFU){
+                        loop_exit = 1U;
+                        func_exit = 1U;
+
+                        res = FR_DISK_ERR;
+                        fp->err = (BYTE)(res);
+
+                        /* Exit */
+                    }
+                    else{
+                        fp->clust = clst;			/* Update current cluster */
+                        if (fp->obj.sclust == 0U){
+                            fp->obj.sclust = clst;	/* Set start cluster if the first write */
+                        }
+                    }
+                }
+
 #if FF_FS_TINY
-			if (fs->winsect == fp->sect && sync_window(fs) != FR_OK) ABORT(fs, FR_DISK_ERR);	/* Write-back sector cache */
+                if(loop_exit == 0U){
+                    if (fs->winsect == fp->sect && sync_window(fs) != FR_OK){
+                        ABORT(fs, FR_DISK_ERR);	/* Write-back sector cache */
+                    }
+                }
 #else
-			if (fp->flag & FA_DIRTY) {		/* Write-back sector cache */
-				if (disk_write(fs->pdrv, fp->buf, fp->sect, 1) != RES_OK) ABORT(fs, FR_DISK_ERR);
-				fp->flag &= (BYTE)~FA_DIRTY;
-			}
+                if(loop_exit == 0U){
+                    if (fp->flag & FA_DIRTY) {		/* Write-back sector cache */
+                        if (disk_write(fs->pdrv, fp->buf, fp->sect, 1U) != RES_OK){
+                            loop_exit = 1U;
+                            func_exit = 1U;
+
+                            res = FR_DISK_ERR;
+                            fp->err = (BYTE)(res);
+
+                            /* Exit */
+                        }
+                        else{
+                            fp->flag &= (BYTE)~FA_DIRTY;
+                        }
+                    }
+                }
 #endif
-			sect = clst2sect(fs, fp->clust);	/* Get current sector */
-			if (sect == 0U) ABORT(fs, FR_INT_ERR);
-			sect += csect;
-			cc = btw / SS(fs);				/* When remaining bytes >= sector size, */
-			if (cc > 0U) {					/* Write maximum contiguous sectors directly */
-				if (csect + cc > fs->csize) {	/* Clip at cluster boundary */
-					cc = fs->csize - csect;
-				}
-				if (disk_write(fs->pdrv, wbuff, sect, cc) != RES_OK) ABORT(fs, FR_DISK_ERR);
+
+                if(loop_exit == 0U){
+                    sect = clst2sect(fs, fp->clust);	/* Get current sector */
+                    if (sect == 0U){
+                        loop_exit = 1U;
+                        func_exit = 1U;
+
+                        res = FR_INT_ERR;
+                        fp->err = (BYTE)(res);
+
+                        /* Exit */
+                    }
+                }
+
+                if(loop_exit == 0U){
+
+                    sect += csect;
+                    cc = btw / SS(fs);				/* When remaining bytes >= sector size, */
+                    if (cc > 0U) {					/* Write maximum contiguous sectors directly */
+
+                        temp_word = fs->csize;
+                        if (csect + cc > temp_word) {	/* Clip at cluster boundary */
+                            cc = fs->csize - csect;
+                        }
+
+                        if (disk_write(fs->pdrv, wbuff, sect, cc) != RES_OK){
+                            loop_exit = 1U;
+                            func_exit = 1U;
+
+                            res = FR_DISK_ERR;
+                            fp->err = (BYTE)(res);
+
+                            /* Exit */
+                        }
+
+                        if(loop_exit == 0U){
 #if FF_FS_MINIMIZE <= 2
 #if FF_FS_TINY
-				if (fs->winsect - sect < cc) {	/* Refill sector cache if it gets invalidated by the direct write */
-					mem_cpy(fs->win, wbuff + ((fs->winsect - sect) * SS(fs)), SS(fs));
-					fs->wflag = 0;
-				}
+                            if (fs->winsect - sect < cc) {	/* Refill sector cache if it gets invalidated by the direct write */
+                                mem_cpy(fs->win, wbuff + ((fs->winsect - sect) * SS(fs)), SS(fs));
+                                fs->wflag = 0;
+                            }
 #else
-				if (fp->sect - sect < cc) { /* Refill sector cache if it gets invalidated by the direct write */
-					mem_cpy(fp->buf, wbuff + ((fp->sect - sect) * SS(fs)), SS(fs));
-					fp->flag &= (BYTE)~FA_DIRTY;
-				}
+                            if (fp->sect - sect < cc) { /* Refill sector cache if it gets invalidated by the direct write */
+                                mem_cpy(fp->buf, wbuff + ((fp->sect - sect) * SS(fs)), SS(fs));
+                                fp->flag &= (BYTE)~FA_DIRTY;
+                            }
 #endif
 #endif
-				wcnt = SS(fs) * cc;		/* Number of bytes transferred */
-				continue;
-			}
-#if FF_FS_TINY
-			if (fp->fptr >= fp->obj.objsize) {	/* Avoid silly cache filling on the growing edge */
-				if (sync_window(fs) != FR_OK) ABORT(fs, FR_DISK_ERR);
-				fs->winsect = sect;
-			}
-#else
-			if ((fp->sect != sect) && 		/* Fill sector cache with file data */
-				(fp->fptr < fp->obj.objsize) &&
-				(disk_read(fs->pdrv, fp->buf, sect, 1) != RES_OK)) {
-					ABORT(fs, FR_DISK_ERR);
-			}
-#endif
-			fp->sect = sect;
-		}
-		wcnt = SS(fs) - (UINT)fp->fptr % SS(fs);	/* Number of bytes left in the sector */
-		if (wcnt > btw) wcnt = btw;					/* Clip it by btw if needed */
-#if FF_FS_TINY
-		if (move_window(fs, fp->sect) != FR_OK) ABORT(fs, FR_DISK_ERR);	/* Move sector window */
-		mem_cpy(fs->win + fp->fptr % SS(fs), wbuff, wcnt);	/* Fit data to the sector */
-		fs->wflag = 1;
-#else
-		mem_cpy(fp->buf + fp->fptr % SS(fs), wbuff, wcnt);	/* Fit data to the sector */
-		fp->flag |= FA_DIRTY;
-#endif
-	}
+                            wcnt = SS(fs) * cc;		/* Number of bytes transferred */
+                            loop_skip = 1U;
+                        }
+                    }
+                }
 
-	fp->flag |= FA_MODIFIED;				/* Set file change flag */
+                if(loop_skip == 0U){
+
+    #if FF_FS_TINY
+                    if(loop_exit == 0U){
+                        if (fp->fptr >= fp->obj.objsize) {	/* Avoid silly cache filling on the growing edge */
+                            if (sync_window(fs) != FR_OK){
+                                loop_exit = 1U;
+                                func_exit = 1U;
+
+                                res = FR_DISK_ERR;
+                                fp->err = (BYTE)(res);
+
+                                /* Exit */
+                            }
+                            else{
+                                fs->winsect = sect;
+                            }
+                        }
+                    }
+    #else
+                    if(loop_exit == 0U){
+                        if ((fp->sect != sect) && 		/* Fill sector cache with file data */
+                            (fp->fptr < fp->obj.objsize)){
+
+                            if(disk_read(fs->pdrv, fp->buf, sect, 1U) != RES_OK) {
+                                loop_exit = 1U;
+                                func_exit = 1U;
+
+                                res = FR_DISK_ERR;
+                                fp->err = (BYTE)(res);
+
+                                /* Exit */
+                            }
+                        }
+                    }
+    #endif
+                    if(loop_exit == 0U){
+                        fp->sect = sect;
+                    }
+                }
+            }
+
+            if(loop_skip == 0U){
+
+                if(loop_exit == 0U){
+                    wcnt = SS(fs) - (UINT)fp->fptr % SS(fs);	/* Number of bytes left in the sector */
+                    if (wcnt > btw){
+                        wcnt = btw;					/* Clip it by btw if needed */
+                    }
+                }
+
+                if(loop_exit == 0U){
+    #if FF_FS_TINY
+                    if (move_window(fs, fp->sect) != FR_OK){
+                        loop_exit = 1U;
+                        func_exit = 1U;
+
+                        res = FR_DISK_ERR;
+                        fp->err = (BYTE)(res);
+
+                        /* Exit */	/* Move sector window */
+                    }
+                    else{
+                        mem_cpy(fs->win + fp->fptr % SS(fs), wbuff, wcnt);	/* Fit data to the sector */
+                        fs->wflag = 1;
+                    }
+    #else
+                    mem_cpy(fp->buf + fp->fptr % SS(fs), wbuff, wcnt);	/* Fit data to the sector */
+                    fp->flag |= FA_DIRTY;
+    #endif
+                }
+            }
+
+            if(loop_exit == 1U){
+                break;
+            }
+            else{
+                *bw += wcnt;
+                wbuff += wcnt;
+                fp->fptr += wcnt;
+
+                fp->obj.objsize = (fp->fptr > fp->obj.objsize) ? fp->fptr : fp->obj.objsize;
+            }
+        }
+
+        if(func_exit == 0U){
+            fp->flag |= FA_MODIFIED;				/* Set file change flag */
+        }
+	}
 
 	LEAVE_FF(fs, FR_OK);
 }
